ccc0392073f6272418a5634d9285e9f67a957629
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-07-26 19:40:39 +0300

    15. Type Assignment & Type Inference

--- 011. Using Types/app.ts
+++ 011. Using Types/app.ts
@@ -11,7 +11,7 @@ function add(
   return sum;
 }
 
-const number1 = 5;
-const number2 = 2.8;
+const number1: number = 5; // we can assignt type explicitely
+const number2 = 2.8; // but it can also be inferred from the value and it is the best practice
 
 const sum = add(number1, number2, true, "The result is");
