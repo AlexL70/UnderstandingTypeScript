1729ded58af888dd0eb0226c37a7db4c8d973963
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-07-26 19:27:26 +0300

    14. Working with Numbers, Strings & Booleans

--- 011. Using Types/app.ts
+++ 011. Using Types/app.ts
@@ -1,12 +1,17 @@
-function add(n1: number, n2: number): number {
-  if (typeof n1 !== "number" || typeof n2 !== "number") {
-    throw new Error("Incorrect input!");
+function add(
+  n1: number,
+  n2: number,
+  logResult: boolean,
+  prefix: string
+): number {
+  const sum = n1 + n2;
+  if (logResult) {
+    console.log(prefix, sum);
   }
-  return n1 + n2;
+  return sum;
 }
 
-const number1 = "5";
+const number1 = 5;
 const number2 = 2.8;
 
-const sum = add(number1, number2);
-console.log(sum);
+const sum = add(number1, number2, true, "The result is");
