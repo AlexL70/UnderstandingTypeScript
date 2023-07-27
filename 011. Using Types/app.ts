878028a380863adf7232813a143324e5583f0cac
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-07-27 15:11:02 +0300

    18. Arrays Types

--- 011. Using Types/app.ts
+++ 011. Using Types/app.ts
@@ -1,9 +1,10 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+const person = {
   name: "Alexander",
   age: 53,
+  hobbies: ["Sports", "Cooking"],
 };
 
 console.log(person.name);
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
