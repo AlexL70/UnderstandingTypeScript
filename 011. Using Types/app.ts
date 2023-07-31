fcfcafd51d648358bc127e338ce7ffaaa0e1002d
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-07-31 11:08:12 +0300

    19. Working with Tuples

--- 011. Using Types/app.ts
+++ 011. Using Types/app.ts
@@ -1,10 +1,16 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  // here TS knows that it is 2-element array is expected and the first one must be a number
+  // and the second one must be a string. The only exception is push() function. TS misses
+  // to check if it does a right thing.
+  role: [number, string];
+} = {
   name: "Alexander",
   age: 53,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
 
-console.log(person.name);
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
+console.log(person);
