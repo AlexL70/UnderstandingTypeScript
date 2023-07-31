3b62ecdbe04ea368458745aa9ff25aeb75c85fd6
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-07-31 12:43:52 +0300

    23. Literal Types

--- 011. Using Types/app.ts
+++ 011. Using Types/app.ts
@@ -1,21 +1,30 @@
 function combine(
   input1: number | string,
-  input2: number | string
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
 ): number | string {
   let combination: number | string;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    combination = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    combination = +input1 + +input2;
   } else if (typeof input1 === "string" && typeof input2 === "string") {
     combination = input1 + input2;
   } else {
     throw new Error("Incompatible types!");
   }
 
-  return combination;
+  return resultConversion == "as-number"
+    ? +combination
+    : combination.toString();
 }
 
-const combinedAges = combine(26, 30);
+const combinedAges = combine(26, 30, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Max", "Anna");
+const combinedStringAges = combine("26", "30", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
