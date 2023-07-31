fc9d9983351d9e42c8a35da48d27d063e078d539
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-07-31 11:27:29 +0300

    20. Working with Enums

--- 011. Using Types/app.ts
+++ 011. Using Types/app.ts
@@ -1,16 +1,22 @@
+enum Role {
+  Admin,
+  ReadOnly,
+  Author,
+}
+
 const person: {
   name: string;
   age: number;
   hobbies: string[];
-  // here TS knows that it is 2-element array is expected and the first one must be a number
-  // and the second one must be a string. The only exception is push() function. TS misses
-  // to check if it does a right thing.
-  role: [number, string];
+  role: Role;
 } = {
   name: "Alexander",
   age: 53,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.Author,
 };
 
 console.log(person);
+if (person.role === Role.Author) {
+  console.log("is author!");
+}
