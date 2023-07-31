ec5f57fd36af698e43522ab4f44fc7a7196270fc
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-07-31 13:14:39 +0300

    27. Functions as Types

--- 011. Using Types/app.ts
+++ 011. Using Types/app.ts
@@ -6,4 +6,10 @@ function printResult(num: number): void {
   console.log(`Result: ${num}`);
 }
 
-console.log(printResult(add(5, 12)));
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
