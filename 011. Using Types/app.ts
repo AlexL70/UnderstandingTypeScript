3a952f2234b6de24e14dd7f563afc78c2818d185
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-08-06 18:42:34 +0300

    30. The never Type

--- 011. Using Types/app.ts
+++ 011. Using Types/app.ts
@@ -8,3 +8,9 @@ userInput = "Alex";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError("An error occured!", 500);
