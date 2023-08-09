aba1f5dc361c493e341a6f13111a3a2b9e871fb0
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-08-09 19:01:26 +0300

    62. "private" and "public" Access Modifiers

--- 059. Creating a First Class/src/app.ts
+++ 059. Creating a First Class/src/app.ts
@@ -1,15 +1,29 @@
 class Department {
-  name: string;
+  private name: string;
+  private employees: string[] = [];
+
   constructor(name: string) {
     this.name = name;
   }
 
-  describe(this: Department) {
+  public describe(this: Department) {
     console.log(`Department: "${this.name}"`);
+  }
+
+  public addEmployee(this: Department, employee: string) {
+    this.employees.push(employee);
+  }
+
+  public printEmployeesInfo(this: Department) {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const dep = new Department("Accounting");
 dep.describe();
-const depCopy = { describe: dep.describe, name: "HR" };
-depCopy.describe();
+//const depCopy = { describe: dep.describe, name: "HR" };
+//depCopy.describe();
+dep.addEmployee("Tom Waits");
+dep.addEmployee("Keith Richard");
+dep.printEmployeesInfo();
