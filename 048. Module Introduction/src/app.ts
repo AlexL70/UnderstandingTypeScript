7f6d148df3149fec98cb579e7cda87e65d93ee56
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-08-09 13:20:29 +0300

    54. Array & Object Destructuring

--- 048. Module Introduction/src/app.ts
+++ 048. Module Introduction/src/app.ts
@@ -1,5 +1,5 @@
 const person = {
-  name: "Alex",
+  firstName: "Alex",
   age: 40,
 };
 
@@ -11,3 +11,10 @@ const add = (...numbers: number[]) =>
   numbers.reduce((result, value) => result + value, 0);
 
 console.log(add(6, 11, 5, 13));
+
+const hobbies = ["Hiking", "Cooking", "Sports", "Driving", "Shopping"];
+const [hobby1, hobby2, hobby3, ...remainings] = hobbies;
+console.log(hobby1, hobby2, hobby3, remainings);
+
+const { firstName: personName, age: personAge } = person;
+console.log(personName, personAge);
