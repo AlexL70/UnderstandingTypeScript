002a43cc30efaa990a50f66c97a85f7a54710cbd
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-08-20 12:54:55 +0300

    77. Interfaces as Function Types

--- 059. Creating a First Class/src/app.ts
+++ 059. Creating a First Class/src/app.ts
@@ -1,7 +1,20 @@
+//  Define function type as an interface
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+// This function definition complies with AddFn interface
+add = (n1: number, n2: number) => n1 + n2;
+
 interface Named {
   readonly name: string;
 }
 
+// Greeteable interface extends Named one meaning it
+// has all properties and methods defined in Named one
+// plus ones defined in itself. One interface can extend
+// several ones
 interface Greetable extends Named {
   greet(phrase: string): void;
 }
