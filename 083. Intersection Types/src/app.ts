e298d506ce23c41e453416a8684cd1744438fd5d
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-08-20 15:27:10 +0300

    84. More on Type Guards

--- 083. Intersection Types/src/app.ts
+++ 083. Intersection Types/src/app.ts
@@ -22,3 +22,55 @@ const e1: ElevatedEmployee = {
 type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric; // means string, number or boolean
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  } else {
+    return a.toString() + b.toString();
+  }
+}
+
+type UnknownEmployee = Employee | Admin;
+function printEmployeeInfo(employee: UnknownEmployee) {
+  console.log(`Name: ${employee.name}`);
+  if ("privileges" in employee) {
+    console.log(`Privileges: ${employee.privileges}`);
+  }
+  if ("startDate" in employee) {
+    console.log(`Start Date: ${employee.startDate}`);
+  }
+}
+
+printEmployeeInfo(e1);
+printEmployeeInfo({ name: "Tom", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving a car...");
+  }
+}
+
+class Track {
+  drive() {
+    console.log("Driving a track...");
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Loading cargo: ${amount}...`);
+  }
+}
+
+type Vehicle = Car | Track;
+const v1 = new Car();
+const v2 = new Track();
+
+function useVehicle(v: Vehicle) {
+  if (v instanceof Track) {
+    v.loadCargo(25);
+  }
+  v.drive();
+}
+
+useVehicle(v1);
+useVehicle(v2);
