71e8ee7990d3ac16aebc6fba9242a7ec3a8c9208
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-09-03 15:44:41 +0300

    85. Discriminated Unions

--- 083. Intersection Types/src/app.ts
+++ 083. Intersection Types/src/app.ts
@@ -74,3 +74,36 @@ function useVehicle(v: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+  type: "bird";
+  flyingSpeen: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  var speed: number;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeen;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+    default:
+      throw new Error("Unknown animal type");
+      break;
+  }
+  console.log(`Moving with speed ${speed}`);
+}
+
+let bird: Bird = { flyingSpeen: 10, type: "bird" };
+let horse: Horse = { runningSpeed: 5, type: "horse" };
+moveAnimal(bird);
+moveAnimal(horse);
