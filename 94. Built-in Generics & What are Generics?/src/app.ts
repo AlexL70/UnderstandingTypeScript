519fa54902e5bb85e211ac4cdb38f4295066ee51
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-09-16 15:18:48 +0300

    94. Built-in Generics & What are Generics?

--- 94. Built-in Generics & What are Generics?/src/app.ts
+++ 94. Built-in Generics & What are Generics?/src/app.ts
@@ -1,1 +1,15 @@
-console.log("Your code goes here...");
+const names: Array<string> = ["Max", "Alex"];
+console.log(names);
+
+const promise: Promise<string> = new Promise((resolve, _) => {
+  setTimeout(() => {
+    resolve("This promise is resolved!");
+  }, 500);
+});
+
+promise.then((data) => {
+  console.log(data.split(" "));
+});
+
+const myFunc = () => promise;
+myFunc();
