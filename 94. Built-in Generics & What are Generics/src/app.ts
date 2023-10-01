00d25a352004850c163bdb8f88eda64e0989c281
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-10-01 19:34:28 +0300

    96. Working with Constraints

--- 94. Built-in Generics & What are Generics/src/app.ts
+++ 94. Built-in Generics & What are Generics/src/app.ts
@@ -14,8 +14,11 @@ promise.then((data) => {
 const myFunc = () => promise;
 myFunc();
 
-function merge<T extends {}, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign<T, U>(objA, objB);
 }
+
+// would give an error because 52 is not an object and it breaks a restriction
+//const merged = merge({ name: "Alex" }, 52);
 const merged = merge({ name: "Alex" }, { age: 52 });
 console.log(merged.name, merged.age);
