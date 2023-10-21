26823d3a1cd2f654204b3e6ca5d268dc7b5cd20e
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-10-21 19:59:16 +0300

    108. Adding Multiple Decorators

--- 105. A First Class Decorator/src/app.ts
+++ 105. A First Class Decorator/src/app.ts
@@ -1,18 +1,22 @@
 function Logger(description: string) {
+  console.log("Logger's factory running...");
   return function (constructor: Function) {
     console.log(`${description}\n${constructor.toString()}`);
   };
 }
 
 function WithTemplate(htmlTemplate: string, hookId: string) {
+  console.log("Template's factory running...");
   return function (_: Function) {
+    console.log("Rendering template...");
     const hookEl = document.getElementById(hookId);
     if (hookEl) {
       hookEl.innerHTML = htmlTemplate;
     }
   };
 }
-//@Logger(`Logging ${Person.name} class definition...`)
+
+@Logger(`Logging ${Person.name} class definition...`)
 @WithTemplate(`<h2>Hey Joe!</h2>`, "app")
 class Person {
   name = "Alex";
