5af1698b6f34f2f10fd29d04a13420d8e2763caa
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-10-21 15:41:00 +0300

    105. A First Class Decorator

--- 105. A First Class Decorator/src/app.ts
+++ 105. A First Class Decorator/src/app.ts
@@ -1,1 +1,15 @@
-console.log("Your code goes here...");
+function Logger(constructor: Function) {
+  console.log(`Logging class definition...\n${constructor.toString()}`);
+}
+@Logger
+class Person {
+  name = "Alex";
+
+  constructor() {
+    console.log("Creating person object.");
+  }
+}
+
+// Decorator runs even before class is instantiated.
+//const person = new Person();
+//console.log(person);
